32fd5657dac8a45e9648b77baf1f883df226d1cd
1 <rkdguswns99@gmail.com>
2024-07-12 13:50:07 +0900

    240712

---
+++ 01_Console/01_Console/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 주석
+            /*
+             * 여러줄
+             */
+
+            ///엔터치면 아래줄 주석
+            ///
+
+            // F9 브레이크포인트
+            // F10 다음줄
+            // F5 디버그, 디버깅 중엔 다음 브레이크포인트까지 진행
+
+            // ctrl + d 현재 코드 복붙
+            // shift + del 현재 줄 지움
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("slsllddfk");
+
+            // 변수 : 데이터를 저장한 곳 (메모리에서의 위치)
+            // 함수 : 특정기능수행 코드덩어리
+            // 클래스 : 특정한 동작을 하는 물체를 표현하기 위해 변수와 함수를 모아 놓은 것
+
+            // 데이터 타입 변수의 종류
+            // int 32bit 42억 사이즈 넘어가면 실행안됨
+            // float 에 정수 값 가능
+            // bool
+            // string
+
+        }
+    }
+}
